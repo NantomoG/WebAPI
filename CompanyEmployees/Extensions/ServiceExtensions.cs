66097204d4b801357e160879fefc0aa259b68753
NantomoG <nantomo1488@gmail.com>
2023-12-22 16:05:54 +0300

    lab1

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+using System.Runtime.ConstrainedExecution;
+using Contracts;
+using LoggerService;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+             services.Configure<IISOptions>(options =>
+             {
+
+             });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
